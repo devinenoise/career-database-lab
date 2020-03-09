15ca818bf84096518d003ef714b44024921fceae
Kyle Devine <devinenoise@gmail.com>
2020-03-09 14:24:46 -0700

    number and string validation passing

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const { isNumber } = require('./lib/types.js');
 const { isString } = require('./lib/types.js');
+const { isBoolean } = require('./lib/types.js');
+const { isArray } = require('./lib/types.js');
 
 
 
 console.log(isNumber('3'));
 console.log(isString('Name'));
+console.log(isBoolean(true));
+console.log(isArray(true));
