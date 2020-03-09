23e09922215b352bdcac358d6a1e7bd280fd1814
Kyle Devine <devinenoise@gmail.com>
2020-03-09 15:24:03 -0700

    casts passing

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
-const { isNumber } = require('./lib/types.js');
-const { isString } = require('./lib/types.js');
-const { isBoolean } = require('./lib/types.js');
-const { isArray } = require('./lib/types.js');
+const { isNumber, isString, isBoolean, isArray, isObject, isFunction, castToString, castToNumber, castToBoolean } = require('./lib/types.js');
+
 
 
 
 console.log(isNumber('3'));
 console.log(isString('Name'));
 console.log(isBoolean(true));
-console.log(isArray(true));
+console.log(isArray([]));
+console.log(isObject({}));
+console.log(isFunction(() => {}));
+
+console.log(castToString('3'));
+console.log(castToNumber(3));
+console.log(castToBoolean(true));
